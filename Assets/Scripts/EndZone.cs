6e87314d7675820610a86710806d6c2acde913e8
SushiDa <ctrn42@gmail.com>
2024-01-28 15:27:26 +0100

    EndZZone repeat

--- Assets/Scripts/EndZone.cs
+++ Assets/Scripts/EndZone.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class EndZone : MonoBehaviour
 {
-    [SerializeField] ParticleSystem ps;
+    [SerializeField] ParticleSystem particles;
     private void OnTriggerEnter(Collider other)
     {
         if(other.TryGetComponent(out PlayerGameController player))
         {
-            ps.Play();
             GameEvents.WaveFinished?.Invoke(true);
+
         }
     }
 }
